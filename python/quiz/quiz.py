9366e287c4444873779453a5005ff511949b991d
FranzPenz <francesco.petrini14@gmail.com>
2025-01-17 08:42:23 +0100

    a

---
+++ python/quiz/quiz.py
@@ -0,0 +1,16 @@
+domande = ("Qual è videogioco ha vinto il titolo di 'Game of the year' nel 2023?: ",
+           "Qual è il personaggio principale della serie di videogiochi Super Mario?",
+           "In quale videogioco devi distruggere blocchi in un mondo a cubetti?",
+           "Qual è la console creata da Sony?",
+           "Qual è il nome completo del protagonista della serie The Legend of Zelda?")
+
+opzioni = (("A. Baldur's Gate III","B. The Legend of Zelda: Tears of the Kingdom","C. Alan Wake 2","D. Resident Evil 4"),
+           ("A. Crash Bandicoot","B. Wario","C. Spyro","D. Nessuna di quelle elencate"),
+           ("A. Minecraft","B. Terraria","C. League of legends","D. F1 2024"),
+           ("A. Playstation","B. Xbox","C. Wii","D. Dreamcast"),
+           ("A. Link","B. Gennaro","C. Pino","D. Paoletto"))
+
+risposte_giuste= ()
+risposte = []
+punteggio = 0
+numero_domanda = 0
