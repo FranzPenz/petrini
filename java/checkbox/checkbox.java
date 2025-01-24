326f2c15b53c7bc37a0f48710444c4ac76f359fe
FranzPenz <francesco.petrini14@gmail.com>
2025-01-24 09:28:44 +0100

    checkbox in java

---
+++ java/checkbox/checkbox.java
@@ -0,0 +1,65 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class checkbox extends JFrame {
+    
+    protected JLabel name = new JLabel("Name: ");
+    protected JLabel surname = new JLabel("Surname: ");
+    protected JTextField text1 = new JTextField("name", 10);
+    protected JTextField text2 = new JTextField("surname", 10);
+    protected JComboBox<String> nationality = new JComboBox<String>();
+    protected JComboBox<String> phone = new JComboBox<String>();
+    protected JCheckBox check1 = new JCheckBox("worker");
+    protected JCheckBox check2 = new JCheckBox("student");
+    protected JPanel p1 = new JPanel();
+    protected JPanel p2 = new JPanel();
+    protected JButton b1 = new JButton("accept");
+    protected JButton b2 = new JButton("cancel");
+
+    public checkbox() {
+        super("Modulo");
+        Container c = this.getContentPane();
+        
+        
+        p1.setLayout(new FlowLayout());
+        p1.add(name);
+        p1.add(text1);
+        p1.add(surname);
+        p1.add(text2);
+
+        
+        phone.addItem("+39");
+        phone.addItem("+112");
+        phone.addItem("+76");
+        phone.addItem("+38");
+        phone.addItem("+221");
+        phone.addItem("+20");
+        p1.add(phone);
+        p1.add(new JTextField(20));
+
+        
+        nationality.addItem("Italian");
+        nationality.addItem("American");
+        nationality.addItem("French");
+        nationality.addItem("German");
+        nationality.addItem("English");
+        nationality.addItem("Spanish");
+        p1.add(nationality);
+        p1.add(check1);
+        p1.add(check2);
+
+        c.add(p1);
+        c.add(p2);
+        c.add(b1);
+        c.add(b2);
+
+        
+        c.add(p1);
+        c.add(p2);
+        c.add(b1);
+        c.add(b2);
+
+        
+        c.setLayout(new GridLayout(4, 1));
+    }
+}
---
+++ java/checkbox/checkbox_exe.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+import javax.swing.*;
+public class checkbox_exe {
+    public static void main(String[] args) {
+        checkbox f = new checkbox();
+        f.setSize(700,300);
+        f.setLocation(100,100);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+    }
+}
