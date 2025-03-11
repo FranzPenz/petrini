991896a69b278d41df7ddc625e0ab75aec863a9b
FranzPenz <francesco.petrini14@gmail.com>
2025-03-11 10:24:37 +0100

    modified thread in java

--- java/thread/implementezioneThread.java
+++ java/thread/implementezioneThread.java
@@ -1,6 +1,6 @@
 public class implementezioneThread {
     public static void main (String[] args) {
-        EsempioThread s = new EsempioThread();
+        EsempioThread s = new EsempioThread(1);
         Thread nuovoThread = new Thread (s);
         nuovoThread.start();
     }
